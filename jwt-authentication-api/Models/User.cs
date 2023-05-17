58f66d80f78c232f099893556ec3ca9e5280a16e
yusufhaydarlok <yusufhaydarlok@gmail.com>
2023-05-17 17:39:37 +0300

    Add project files.

---
+++ jwt-authentication-api/Models/User.cs
@@ -0,0 +1,12 @@
+﻿namespace jwt_authentication_api.Models
+{
+    public class User
+    {
+        public string Username { get; set; } = string.Empty;
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+        public string RefreshToken { get; set; } = string.Empty;
+        public DateTime TokenCreated { get; set; } = DateTime.Now;
+        public DateTime TokenExpires { get; set; }
+    }
+}
---
+++ jwt-authentication-api/Services/IUserService.cs
@@ -0,0 +1,7 @@
+﻿namespace jwt_authentication_api.Services
+{
+    public interface IUserService
+    {
+        string GetMyName();
+    }
+}
